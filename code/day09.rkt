517b83ec4063cda8e96944e9b40d1c978b419c94
Stephen Mizell <smizell@gmail.com>
2021-12-09 11:04:08 -0600

    Day 09 Part 2

--- code/day09.rkt
+++ code/day09.rkt
@@ -3,13 +3,13 @@
 (require rackunit
          threading)
 
-(define (load-map filename)
+(define (load-heightmap filename)
   (~>> filename
        file->string
        (string-split _ "\n")
        (map (λ (l) (~>> l string->list (map string) (map string->number))))))
 
-(struct $pos (x y))
+(struct $pos (x y) #:transparent)
 
 (define (adjacent-poss pos)
   (match-define ($pos x y) pos)
@@ -34,23 +34,57 @@
   (~> heightmap (list-ref ($pos-y pos)) (list-ref ($pos-x pos))))
 
 (define (find-lowest heightmap)
-  (for/fold ([acc '()])
-            ([(vs y) (in-indexed heightmap)])
-    (for/fold ([acc acc])
-              ([(v x) (in-indexed vs)])
-      (define is-smallest
-        (~>> ($pos x y)
-             (adjacent-poss-on-map heightmap)
-             (map (curry pos->value heightmap))
-             (andmap (curry < v))))
-      (if is-smallest (cons v acc) acc))))
+  (for*/fold ([acc '()])
+             ([(vs y) (in-indexed heightmap)]
+              [(v x) (in-indexed vs)])
+    (define pos ($pos x y))
+    (define is-smallest
+      (~>> pos
+           (adjacent-poss-on-map heightmap)
+           (map (curry pos->value heightmap))
+           (andmap (curry < v))))
+    (if is-smallest (cons pos acc) acc)))
 
 (define (part1 filename)
-  (~>> filename
-       load-map
+  (define heightmap (~>> filename load-heightmap))
+  (~>> heightmap
        find-lowest
+       (map (curry pos->value heightmap))
        (map add1)
        (apply +)))
 
 (module+ test
   (check-eq? (part1 "../inputs/day09.txt") 458))
+
+(define (in-basin? heightmap pos)
+  (< (pos->value heightmap pos) 9))
+
+(define (find-basin heightmap pos [basin '()])
+  (define ps (adjacent-poss-on-map heightmap pos))
+  (define bps (filter (curry in-basin? heightmap) ps))
+  (define unexplored (set->list (set-subtract (list->set bps) (list->set basin))))
+  (define new-basin
+    (cond
+      [(in-basin? heightmap pos) (cons pos (append unexplored basin))]
+      [else (append unexplored basin)]))
+  (for/fold ([acc new-basin])
+            ([p (in-list unexplored)])
+    (find-basin heightmap p acc)))
+
+(define (part2 filename)
+  (define heightmap (~>> filename load-heightmap))
+  (apply * (~> heightmap
+               find-lowest
+               (map (λ (p)
+                      ; This sucks but works
+                      (set->list (list->set (find-basin heightmap p))))
+                    _)
+               (map (λ (b)
+                      (map (curry pos->value heightmap) b))
+                    _)
+               (map length _)
+               (sort >)
+               (take 3))))
+
+(module+ test
+  (check-eq? (part2 "../inputs/day09.txt") 1391940))
